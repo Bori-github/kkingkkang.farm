34a0b2234a6b4d8d7007b258b20b6f7722dfd9de
Bori-github <85009583+Bori-github@users.noreply.github.com>
2022-01-14 01:37:28 +0900

    :sparkles: Login 페이지 생성

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,35 @@
 import type { NextPage } from 'next';
+import Head from 'next/head';
+import Link from 'next/link';
 
-const Home: NextPage = () => {
+const Login: NextPage = () => {
   return (
-    <div>
-      <h1>H1</h1>
-      <div>Home</div>
-    </div>
+    <>
+      <Head>
+        <title>로그인ㅣ낑깡팜</title>
+      </Head>
+      <main>
+        <figure>
+          <img src="/logo/logo-white.png" alt="낑깡팜 로고" />
+        </figure>
+        <section>
+          <form>
+            <button type="button">카카오톡 계정으로 로그인</button>
+            <button type="button">구글 계정으로 로그인</button>
+            <button type="button">페이스북 계정으로 로그인</button>
+          </form>
+          <ul>
+            <li>
+              <Link href="/">이메일로 로그인</Link>
+            </li>
+            <li>
+              <Link href="/">회원가입</Link>
+            </li>
+          </ul>
+        </section>
+      </main>
+    </>
   );
 };
 
-export default Home;
+export default Login;
