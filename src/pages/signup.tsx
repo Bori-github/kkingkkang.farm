9eb8ad5260fe53e67fa97e26a1de075d32e02b4c
Bori-github <85009583+Bori-github@users.noreply.github.com>
2022-01-15 01:49:26 +0900

    :lipstick: Signup 페이지 CSS 적용

--- src/pages/signup.tsx
+++ src/pages/signup.tsx
@@ -1,5 +1,70 @@
+import styled from '@emotion/styled';
 import { NextPage } from 'next';
 import Head from 'next/head';
+import { BORDER, BUTTON, COLORS } from '../constants';
+
+const MainSignup = styled.main`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 30px 34px;
+`;
+const TitleMain = styled.h2`
+  margin-bottom: 40px;
+  color: ${COLORS.black};
+  font-size: 24px;
+  font-weight: 700;
+`;
+const FormLogin = styled.form`
+  width: 100%;
+`;
+const BoxInp = styled.div`
+  &:not(:first-of-type) {
+    margin-top: 16px;
+  }
+`;
+const Label = styled.label`
+  font-size: 12px;
+
+  & input {
+    display: block;
+    width: 100%;
+    padding: 10px 0 8px;
+    border: 0;
+    border-bottom: ${BORDER.basic};
+    color: ${COLORS.black};
+    font-size: 14px;
+  }
+  & input::placeholder {
+    color: ${COLORS.placeholder};
+  }
+  & input:focus,
+  & input:active {
+    border-color: ${COLORS.accent_green};
+  }
+`;
+const TxtError = styled.span`
+  display: inline-block;
+  margin-top: 6px;
+  color: ${COLORS.error};
+  font-size: 12px;
+`;
+const BtnNext = styled.button`
+  width: 100%;
+  margin: 30px 0 20px;
+  padding: 13px 0;
+  border: ${BUTTON.border};
+  border-radius: 44px;
+  color: ${BUTTON.color};
+  font-weight: 700;
+  transition: all 0.3s;
+
+  &:hover,
+  &:active {
+    background-color: ${BUTTON.background_color};
+    color: ${BUTTON.active_color};
+  }
+`;
 
 const LoginEmail: NextPage = () => {
   return (
@@ -7,11 +72,11 @@ const LoginEmail: NextPage = () => {
       <Head>
         <title>이메일로 회원가입ㅣ낑깡팜</title>
       </Head>
-      <main>
-        <h2>이메일로 회원가입</h2>
-        <form>
-          <div>
-            <label htmlFor="email">
+      <MainSignup>
+        <TitleMain>이메일로 회원가입</TitleMain>
+        <FormLogin>
+          <BoxInp>
+            <Label htmlFor="email">
               이메일
               <input
                 type="email"
@@ -19,11 +84,11 @@ const LoginEmail: NextPage = () => {
                 placeholder="이메일 주소를 입력해 주세요."
                 required
               />
-            </label>
-            <span>*이미 가입된 이메일 주소입니다.</span>
-          </div>
-          <div>
-            <label htmlFor="password">
+            </Label>
+            <TxtError>*이미 가입된 이메일 주소입니다.</TxtError>
+          </BoxInp>
+          <BoxInp>
+            <Label htmlFor="password">
               비밀번호
               <input
                 type="password"
@@ -31,12 +96,12 @@ const LoginEmail: NextPage = () => {
                 placeholder="비밀번호를 설정해 주세요."
                 required
               />
-            </label>
-            <span>*비밀번호는 6자 이상이어야 합니다.</span>
-          </div>
-          <button type="submit">다음</button>
-        </form>
-      </main>
+            </Label>
+            <TxtError>*비밀번호는 6자 이상이어야 합니다.</TxtError>
+          </BoxInp>
+          <BtnNext type="submit">다음</BtnNext>
+        </FormLogin>
+      </MainSignup>
     </>
   );
 };
