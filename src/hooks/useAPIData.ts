a438fefd57b5fb27ffaedaf187ce8a5ca7e44a01
Bori-github <qhflrnfl4324@gmail.com>
2022-02-12 23:45:04 +0900

    🚚 index.ts > useAPIData.ts 파일명 변경

--- src/hooks/useAPIData.ts
+++ src/hooks/useAPIData.ts
@@ -2,6 +2,6 @@ import useSWR from 'swr';
 import { fetcher } from '../utils/fetcher';
 import { API_ENDPOINT } from '../constants';
 
-export const useApiData = (path: string) => {
+export const useAPIData = (path: string) => {
   return useSWR(`${API_ENDPOINT}${path}`, fetcher);
 };
