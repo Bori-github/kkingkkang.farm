6378e72e4397eb49215f78ea52029db4ed127281
Bori-github <qhflrnfl4324@gmail.com>
2022-03-05 20:21:36 +0900

    :fire: 불필요한 import 삭제

--- src/pages/list-followers/[id].tsx
+++ src/pages/list-followers/[id].tsx
@@ -1,13 +1,9 @@
 import styled from '@emotion/styled';
 import { NextPage } from 'next';
 import Head from 'next/head';
-import { useRouter } from 'next/router';
-import useSWR from 'swr';
 import { FollowerContainer } from '../../components/follow/FollowerContainer';
 import { HeaderListFollowers } from '../../components/layouts/Header';
 import { Navigation } from '../../components/layouts/Navigation';
-import { API_ENDPOINT } from '../../constants';
-import { fetcher } from '../../utils/fetcher';
 
 const UserListFollowers: NextPage = () => {
   return (
