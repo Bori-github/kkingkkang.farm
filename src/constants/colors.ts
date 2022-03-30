79dc708aff860e9feb1242cbb66218332d80afbb
Bori-github <qhflrnfl4324@gmail.com>
2022-03-30 18:54:23 +0900

    :recycle: 공통 컬러 팔레트 분리 및 색상 추가

---
+++ src/constants/colors.ts
@@ -0,0 +1,16 @@
+export const PRIMARY = '#528540';
+export const SECONDARY = '#CDDE47';
+
+export const GRAY_900 = '#1B1B1B';
+export const GRAY_800 = '#212121';
+export const GRAY_700 = '#484848';
+export const GRAY_600 = '#757575';
+export const GRAY_500 = '#9E9E9E';
+export const GRAY_400 = '#BDBDBD';
+export const GRAY_300 = '#E0E0E0';
+export const GRAY_200 = '#EEEEEE';
+export const GRAY_100 = '#F5F5F5';
+export const GRAY_50 = '#FAFAFA';
+export const WHITE = '#FFFFFF';
+
+export const ERROR = '#EB5757';
