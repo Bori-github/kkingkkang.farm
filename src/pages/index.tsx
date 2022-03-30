f7d7e0f2f6ac26f25f8e8c2792e58b6ac2e5fe2b
Bori-github <qhflrnfl4324@gmail.com>
2022-03-31 01:22:24 +0900

    :fire: 동작하지 않는 로그인 코드 삭제

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -15,26 +15,6 @@ const Login: NextPage = () => {
         <FigLogo>
           <img src="/logo/logo.png" alt="낑깡팜 로고" />
         </FigLogo>
-        <SectionLogin>
-          <h2 className="sr-only">낑깡팜 로그인 창</h2>
-          <FormSnsLogin>
-            <BtnSnsLogin type="button">카카오톡 계정으로 로그인</BtnSnsLogin>
-            <BtnSnsLogin type="button">구글 계정으로 로그인</BtnSnsLogin>
-            <BtnSnsLogin type="button">페이스북 계정으로 로그인</BtnSnsLogin>
-          </FormSnsLogin>
-          <ListLink>
-            <li>
-              <Link href="/login-email">
-                <a>이메일로 로그인</a>
-              </Link>
-            </li>
-            <li>
-              <Link href="/signup">
-                <a>회원가입</a>
-              </Link>
-            </li>
-          </ListLink>
-        </SectionLogin>
       </MainLogin>
       <SplashScreen />
     </>
@@ -56,60 +36,13 @@ const FigLogo = styled.figure`
   margin-bottom: 291px;
 `;
 
-const SectionLogin = styled.section`
-  position: absolute;
-  right: 0;
-  bottom: 0;
-  left: 0;
-  padding: 45px 34px;
-  border-radius: 20px 20px 0 0;
-  background-color: #fff;
-`;
-const FormSnsLogin = styled.form`
-  display: flex;
-  flex-direction: column;
-`;
-const BtnSnsLogin = styled.button`
-  width: 100%;
-  margin: 5px 0;
-  padding: 13px 0;
-  border: 1px solid #f2c94c;
-  border-radius: 44px;
-  background: url('/sns/kakao.png') no-repeat 17px 50%;
-  background-size: 24px 24px;
-
-  &:nth-of-type(2) {
-    border-color: #767676;
-    background-image: url('/sns/google.png');
-  }
-  &:nth-of-type(3) {
-    border-color: #2d9cdb;
-    background-image: url('/sns/facebook.png');
-  }
-`;
-const ListLink = styled.ul`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  margin: 10px 0;
-  font-size: 12px;
-
-  & li:nth-of-type(1)::after {
-    content: '';
-    display: inline-block;
-    width: 1px;
-    height: 12px;
-    margin: 0 12px;
-    background-color: #c4c4c4;
-  }
-`;
-
 const splash = keyframes`
   to {
     opacity: 0;
     visibility: hidden;
   }
 `;
+
 const SplashScreen = styled.div`
   position: fixed;
   top: 0;
