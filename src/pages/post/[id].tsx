c459cb79052dc2f5c98481f195be2a395dc8b8d1
Bori-github <qhflrnfl4324@gmail.com>
2022-04-14 00:46:58 +0900

    :recycle: Post API 연결 및 PostCard 컴포넌트로 데이터 전달

--- src/pages/post/[id].tsx
+++ src/pages/post/[id].tsx
@@ -2,27 +2,59 @@ import styled from '@emotion/styled';
 import { NextPage } from 'next';
 import Head from 'next/head';
 import { useRouter } from 'next/router';
-import { useEffect } from 'react';
 import useSWR from 'swr';
 import { Loader } from '../../components/common/Loader';
 import { HeaderUserPage } from '../../components/layouts/Header';
 import { PostCard } from '../../components/post/PostCard';
 import { SectionInpReply } from '../../components/SectionInput';
 import { SectionReplies } from '../../components/SectionReplies';
-import { UserAvatar } from '../../components/UserAvatar';
-import { API_ENDPOINT, USER_AVATAR } from '../../constants';
-import { GRAY_900 } from '../../constants/colors';
+import { API_ENDPOINT } from '../../constants';
 import { fetcher } from '../../utils';
 
 const Post: NextPage = () => {
+  const router = useRouter();
+  const { id } = router.query;
+  const { data, error } = useSWR(
+    id ? `${API_ENDPOINT}/post/${id}` : null,
+    fetcher,
+  );
+
+  if (!data) return <Loader height="calc(100vh - 109px)" />;
+  if (error) return <div>에러가 발생했습니다.</div>;
+
+  const {
+    id: postID,
+    content,
+    image,
+    createdAt,
+    hearted,
+    heartCount,
+    commentCount,
+    author,
+  } = data.post;
+  const { accountname, username } = author;
+
   return (
     <>
       <Head>
-        <title>유저 이름(유저 아이디)ㅣ낑깡팜</title>
+        <title>
+          {username}(@{accountname})ㅣ낑깡팜
+        </title>
       </Head>
       <HeaderUserPage headerTitle="게시물" />
       <MainPost>
-        <PostCard />
+        <PostCard
+          postData={{
+            id: postID,
+            content,
+            image,
+            createdAt,
+            hearted,
+            heartCount,
+            commentCount,
+            author,
+          }}
+        />
         <SectionReplies />
         <SectionInpReply />
       </MainPost>
