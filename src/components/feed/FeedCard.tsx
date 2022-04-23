08507e6cf7f42ce923f995ff6c1df04a3d7594ed
Bori-github <qhflrnfl4324@gmail.com>
2022-04-23 17:59:07 +0900

    :sparkles: 게시글 더보기 버튼 클릭 시 모달 적용 및 내 게시글 삭제 기능 구현

--- src/components/feed/FeedCard.tsx
+++ src/components/feed/FeedCard.tsx
@@ -2,9 +2,9 @@ import styled from '@emotion/styled';
 import axios from 'axios';
 import Cookies from 'js-cookie';
 import { useRouter } from 'next/router';
-import { useState } from 'react';
-import { API_ENDPOINT, USER_AVATAR } from '../../constants';
-import { GRAY_900 } from '../../constants/colors';
+import { MouseEvent, useRef, useState } from 'react';
+import { API_ENDPOINT, BORDER, USER_AVATAR, Z_INDEX } from '../../constants';
+import { GRAY_300, GRAY_900, PRIMARY, WHITE } from '../../constants/colors';
 import { dateFormatter } from '../../utils';
 import { UserAvatar } from '../UserAvatar';
 import { ImgCarousel } from './ImgCarousel';
@@ -30,8 +30,13 @@ interface FeedCardProps {
   liked: boolean;
 }
 
+interface ModalProps {
+  isShowModal: boolean;
+}
+
 export const FeedCard = ({ postData }: PostProps) => {
   const router = useRouter();
+  const accountname = Cookies.get('accountname');
   const token = Cookies.get('token');
 
   const {
@@ -44,10 +49,19 @@ export const FeedCard = ({ postData }: PostProps) => {
     commentCount,
     author,
   } = postData;
-  const { accountname, username, image: profileImg } = author;
+  const {
+    accountname: authorAccountname,
+    username,
+    image: profileImg,
+  } = author;
+  const postImgList = image && image.split(',');
 
   const [liked, setLiked] = useState(hearted);
   const [likeCount, setLikeCount] = useState(heartCount);
+
+  const [isShowPopup, setIsShowPopup] = useState(false);
+  const [isShowModal, setIsShowModal] = useState(false);
+  const modalRef = useRef<(HTMLElement | null)[]>([]);
 
   const handleBtnLike = async () => {
     if (liked) {
@@ -75,45 +89,139 @@ export const FeedCard = ({ postData }: PostProps) => {
     setLiked(!liked);
   };
 
-  const postImgList = image && image.split(',');
+  const handleCloseModal = (e: MouseEvent<HTMLElement>) => {
+    modalRef.current.forEach((el) => {
+      if (el === e.target) {
+        setIsShowModal(false);
+        setIsShowPopup(false);
+      }
+    });
+  };
+
+  const handleDeletePost = async () => {
+    const { data } = await axios(`${API_ENDPOINT}/post/${postID}`, {
+      method: 'DELETE',
+      headers: {
+        Authorization: `Bearer ${token}`,
+        'Content-type': 'application/json',
+      },
+    });
+
+    if (data.message === '삭제되었습니다.') {
+      setIsShowModal(false);
+      setIsShowPopup(false);
+      // router.push('/user-page');
+    }
+  };
 
   return (
-    <Feed>
-      <BoxProfileImg>
-        <UserAvatar size={USER_AVATAR.sm.size} src={profileImg} />
-      </BoxProfileImg>
-      <HeaderArticle>
-        <UserName>{username}</UserName>
-        <UserId>@{accountname}</UserId>
-        <BtnMore type="button">
-          <span className="sr-only">옵션 더 보기</span>
-        </BtnMore>
-      </HeaderArticle>
-      <div>
-        <TxtFeed>{content}</TxtFeed>
-        {postImgList && postImgList[0] !== '' && (
-          <ImgCarousel imgList={postImgList} />
-        )}
-        <ListIcons>
-          <ItemIcon>
-            <BtnLike type="button" liked={liked} onClick={handleBtnLike}>
-              <span className="sr-only">좋아요</span>
-            </BtnLike>
-            <span>{likeCount}</span>
-          </ItemIcon>
-          <ItemIcon>
-            <BtnReply
-              type="button"
-              onClick={() => router.push(`/post/${postID}`)}
-            >
-              <span className="sr-only">댓글</span>
-            </BtnReply>
-            <span>{commentCount}</span>
-          </ItemIcon>
-        </ListIcons>
-        <TxtCreateAt>{dateFormatter(createdAt)}</TxtCreateAt>
-      </div>
-    </Feed>
+    <>
+      <Feed>
+        <BoxProfileImg>
+          <UserAvatar size={USER_AVATAR.sm.size} src={profileImg} />
+        </BoxProfileImg>
+        <HeaderArticle>
+          <UserName>{username}</UserName>
+          <UserId>@{authorAccountname}</UserId>
+          <BtnMore type="button" onClick={() => setIsShowModal(true)}>
+            <span className="sr-only">더 보기</span>
+          </BtnMore>
+        </HeaderArticle>
+        <div>
+          <TxtFeed>{content}</TxtFeed>
+          {postImgList && postImgList[0] !== '' && (
+            <ImgCarousel imgList={postImgList} />
+          )}
+          <ListIcons>
+            <ItemIcon>
+              <BtnLike type="button" liked={liked} onClick={handleBtnLike}>
+                <span className="sr-only">좋아요</span>
+              </BtnLike>
+              <span>{likeCount}</span>
+            </ItemIcon>
+            <ItemIcon>
+              <BtnReply
+                type="button"
+                onClick={() => router.push(`/post/${postID}`)}
+              >
+                <span className="sr-only">댓글</span>
+              </BtnReply>
+              <span>{commentCount}</span>
+            </ItemIcon>
+          </ListIcons>
+          <TxtCreateAt>{dateFormatter(createdAt)}</TxtCreateAt>
+        </div>
+      </Feed>
+      {isShowModal && (
+        <BgPopup
+          ref={(el) => {
+            modalRef.current[0] = el;
+          }}
+          onClick={handleCloseModal}
+        >
+          {!isShowPopup ? (
+            <ModalPopup isShowModal={isShowModal}>
+              <ul>
+                {authorAccountname === accountname ? (
+                  <>
+                    <ItemMore>
+                      <button
+                        type="button"
+                        onClick={() => setIsShowPopup(true)}
+                      >
+                        삭제
+                      </button>
+                    </ItemMore>
+                    <ItemMore>
+                      <button type="button">수정</button>
+                    </ItemMore>
+                  </>
+                ) : (
+                  <ItemMore onClick={() => setIsShowPopup(true)}>
+                    <button type="button">신고하기</button>
+                  </ItemMore>
+                )}
+              </ul>
+            </ModalPopup>
+          ) : (
+            <Popup>
+              {authorAccountname === accountname ? (
+                <TxtLogout>게시글을 삭제할까요?</TxtLogout>
+              ) : (
+                <TxtLogout>게시글을 신고할까요?</TxtLogout>
+              )}
+              <ListModalBtns>
+                <li>
+                  <BtnCancel
+                    type="button"
+                    ref={(el) => {
+                      modalRef.current[1] = el;
+                    }}
+                    onClick={handleCloseModal}
+                  >
+                    취소
+                  </BtnCancel>
+                </li>
+                <li>
+                  {authorAccountname === accountname ? (
+                    <BtnDelete type="button" onClick={handleDeletePost}>
+                      삭제
+                    </BtnDelete>
+                  ) : (
+                    <BtnDelete
+                      type="button"
+                      // onClick={handleReportPost}
+                    >
+                      신고
+                    </BtnDelete>
+                  )}
+                </li>
+              </ListModalBtns>
+            </Popup>
+          )}
+        </BgPopup>
+      )}
+    </>
   );
 };
 
@@ -201,4 +309,88 @@ const BtnReply = styled.button`
   margin-right: 5px;
   background: url('/icons/message-sm.svg') no-repeat;
   background-size: 100%;
+`;
+
+const BgPopup = styled.div`
+  position: fixed;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  z-index: ${Z_INDEX.popup};
+  background-color: rgba(0, 0, 0, 0.5);
+  color: ${GRAY_900};
+`;
+
+const ModalPopup = styled.div<ModalProps>`
+  position: fixed;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  padding: 36px 0 10px;
+  border-radius: 20px 20px 0 0;
+  background-color: ${WHITE};
+  transform: ${(isShowModal) =>
+    isShowModal ? 'translateY(0)' : 'translateY(100%)'};
+  transition: transform 0.1s;
+  will-change: transform;
+
+  &::before {
+    content: '';
+    position: absolute;
+    top: 16px;
+    left: 50%;
+    width: 50px;
+    height: 4px;
+    border-radius: 4px;
+    background-color: ${GRAY_300};
+    transform: translateX(-50%);
+  }
+`;
+
+const ItemMore = styled.li`
+  padding: 14px 25px;
+  font-size: 14px;
+`;
+
+const Popup = styled.div`
+  position: absolute;
+  top: calc(50% - 30px);
+  left: 50%;
+  border-radius: 10px;
+  background-color: ${WHITE};
+  text-align: center;
+  transform: translate(-50%, -50%);
+`;
+
+const TxtLogout = styled.p`
+  padding: 20px 0;
+  border-bottom: ${BORDER.basic};
+`;
+
+const ListModalBtns = styled.ul`
+  display: flex;
+  font-size: 14px;
+`;
+
+const BtnCancel = styled.button`
+  position: relative;
+  width: 126px;
+  padding: 15px 0;
+
+  &::after {
+    content: '';
+    position: absolute;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    width: 1px;
+    background-color: ${GRAY_300};
+  }
+`;
+
+const BtnDelete = styled.button`
+  width: 126px;
+  padding: 15px 0;
+  color: ${PRIMARY};
 `;
