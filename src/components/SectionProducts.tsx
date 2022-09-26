de21c4b46643ba3cea4a75427eff2b9b596cdc42
Bori-github <qhflrnfl4324@gmail.com>
2022-09-26 23:29:42 +0900

    :lipstick: 프로필 > 판매중인 상품 영역 UI 수정

--- src/components/SectionProducts.tsx
+++ src/components/SectionProducts.tsx
@@ -1,7 +1,9 @@
 import styled from '@emotion/styled';
+import Link from 'next/link';
 import useSWR from 'swr';
 import { API_ENDPOINT } from '../constants';
 import { GRAY_900, PRIMARY, WHITE } from '../constants/colors';
+import { ProductData } from '../types/ProductData';
 import { fetcher } from '../utils';
 import { Loader } from './common/Loader';
 
@@ -26,40 +28,18 @@ export const SectionProducts = ({ accountname }: SectionProductsProps) => {
         <Container>
           <TitleProducts>판매중인 상품</TitleProducts>
           <div>
-            <ListProducts>
-              <ItemProduct>
-                <ImgProduct
-                  src="/example/product-img-example.png"
-                  alt="상품 이미지"
-                />
-                <TitleProduct>낑깡낑깡</TitleProduct>
-                <PriceProduct>35,000원</PriceProduct>
-              </ItemProduct>
-              <ItemProduct>
-                <ImgProduct
-                  src="/example/product-img-example.png"
-                  alt="상품 이미지"
-                />
-                <TitleProduct>낑깡낑깡</TitleProduct>
-                <PriceProduct>35,000원</PriceProduct>
-              </ItemProduct>
-              <ItemProduct>
-                <ImgProduct
-                  src="/example/product-img-example.png"
-                  alt="상품 이미지"
-                />
-                <TitleProduct>낑깡 10kg</TitleProduct>
-                <PriceProduct>45,000원</PriceProduct>
-              </ItemProduct>
-              <ItemProduct>
-                <ImgProduct
-                  src="/example/product-img-example.png"
-                  alt="상품 이미지"
-                />
-                <TitleProduct>낑깡 파지</TitleProduct>
-                <PriceProduct>25,000원</PriceProduct>
-              </ItemProduct>
-            </ListProducts>
+            <List>
+              {product.map((item: ProductData) => {
+                const { id, itemName, price, itemImage, link } = item;
+                return (
+                  <Item key={`product-list-${id}`}>
+                    <ImgProduct src={itemImage} alt={`${itemName} 이미지`} />
+                    <TitleProduct>{itemName}</TitleProduct>
+                    <PriceProduct>{price}원</PriceProduct>
+                  </Item>
+                );
+              })}
+            </List>
           </div>
         </Container>
       )}
@@ -72,6 +52,7 @@ const Container = styled.section`
   padding: 20px;
   background-color: ${WHITE};
 `;
+
 const TitleProducts = styled.h2`
   margin-bottom: 15px;
   color: ${GRAY_900};
@@ -79,14 +60,12 @@ const TitleProducts = styled.h2`
   font-weight: 700;
 `;
 
-const ListProducts = styled.ul`
-  display: flex;
+const List = styled.ul`
   overflow-x: auto;
+  display: flex;
 `;
 
-const ItemProduct = styled.li`
-  display: flex;
-  flex-direction: column;
+const Item = styled.li`
   min-width: 140px;
 
   &:not(:first-of-type) {
@@ -95,11 +74,14 @@ const ItemProduct = styled.li`
 `;
 
 const ImgProduct = styled.img`
-  width: 100%;
+  width: 140px;
+  height: 90px;
   border-radius: 8px;
+  object-fit: cover;
 `;
 
-const TitleProduct = styled.strong`
+const TitleProduct = styled.p`
+  margin: 0;
   padding: 8px 0 6px;
   color: ${GRAY_900};
   font-size: 14px;
