509d997ef3abbd8ec9466b6e2ba3e75aab1b0e94
Bori-github <qhflrnfl4324@gmail.com>
2022-09-26 22:13:02 +0900

    :label: ProductData 추가

--- src/pages/add-product.tsx
+++ src/pages/add-product.tsx
@@ -1,120 +1,120 @@
 import styled from '@emotion/styled';
 import { NextPage } from 'next';
 import Head from 'next/head';
+import { useForm } from 'react-hook-form';
+import { Navigation } from '../components/layouts/Navigation';
 import { ToolBar } from '../components/layouts/ToolBar';
-import { BORDER } from '../constants';
-import {
-  GRAY_300,
-  GRAY_600,
-  GRAY_900,
-  PRIMARY,
-  SECONDARY,
-} from '../constants/colors';
+import { BORDER, BUTTON } from '../constants';
+import { GRAY_200, GRAY_300, GRAY_900, PRIMARY } from '../constants/colors';
+import { ProductData } from '../types/ProductData';
 
 const AddProduct: NextPage = () => {
+  const { register } = useForm<ProductData>({ mode: 'onChange' });
   return (
     <>
       <Head>
         <title>상품 등록ㅣ낑깡팜</title>
       </Head>
-      <ToolBar title="상품 등록" />
-      <MainAddProduct>
-        <section>
-          <h2 className="sr-only">상품 등록 페이지</h2>
-          <FormAddProduct action="#">
-            <BoxInp>
-              <Label htmlFor="upload-img">
-                이미지 등록
-                <BoxUploadImg>
-                  <span>이미지 미리보기</span>
-                  {/* <img src="" alt="" /> */}
-                  <input type="file" id="uploadImg" accept="img/*" required />
-                  <BtnUpload type="button">
-                    <span className="sr-only">파일 선택 버튼</span>
-                  </BtnUpload>
-                </BoxUploadImg>
-              </Label>
-            </BoxInp>
-            <BoxInp>
-              <Label htmlFor="nameProduct">
-                상품명
-                <input
-                  type="text"
-                  id="nameProduct"
-                  placeholder="2~15자 이내여야 합니다."
-                  minLength={2}
-                  maxLength={15}
-                  required
-                />
-              </Label>
-            </BoxInp>
-            <BoxInp>
-              <Label htmlFor="priceProduct">
-                가격
-                <input
-                  type="number"
-                  id="priceProduct"
-                  placeholder="숫자만 입력 가능합니다."
-                  required
-                />
-              </Label>
-            </BoxInp>
-            <BoxInp>
-              <Label htmlFor="urlProduct">
-                판매 링크
-                <input
-                  type="text"
-                  id="urlProduct"
-                  placeholder="URL을 입력해주세요."
-                  required
-                />
-              </Label>
-            </BoxInp>
-          </FormAddProduct>
-        </section>
-      </MainAddProduct>
+      <Section>
+        <h2 className="sr-only">상품 등록 페이지</h2>
+        <Form>
+          <ToolBar title="상품 등록">
+            <button type="button">등록</button>
+          </ToolBar>
+          <BoxInp>
+            <label htmlFor="itemImage">이미지 등록</label>
+            <BoxUploadImg>
+              <span>이미지 미리보기</span>
+              {/* <img src="" alt="" /> */}
+              <input
+                type="file"
+                id="itemImage"
+                accept="image/*"
+                className="sr-only"
+                required
+                {...register('itemImage')}
+              />
+              <LabelUploadImage htmlFor="uploadImg">
+                <span className="sr-only">사진 업로드 버튼</span>
+              </LabelUploadImage>
+            </BoxUploadImg>
+          </BoxInp>
+          <BoxInp>
+            <label htmlFor="itemName">상품명</label>
+            <Input
+              type="text"
+              id="itemName"
+              placeholder="2~15자 이내여야 합니다."
+              minLength={2}
+              maxLength={15}
+              required
+              {...register('itemName')}
+            />
+          </BoxInp>
+          <BoxInp>
+            <label htmlFor="price">가격</label>
+            <Input
+              type="number"
+              id="price"
+              placeholder="숫자만 입력 가능합니다."
+              required
+              {...register('price')}
+            />
+          </BoxInp>
+          <BoxInp>
+            <label htmlFor="link">판매 링크</label>
+            <Input
+              type="text"
+              id="link"
+              placeholder="URL을 입력해주세요."
+              required
+              {...register('link')}
+            />
+          </BoxInp>
+        </Form>
+      </Section>
+      <Navigation />
     </>
   );
 };
 
 export default AddProduct;
 
-const MainAddProduct = styled.main`
+const Section = styled.section`
   margin-top: 49px;
   padding: 30px 34px;
 `;
 
-const FormAddProduct = styled.form`
+const Form = styled.form`
   width: 100%;
+  font-size: 12px;
 `;
+
 const BoxInp = styled.div`
   &:not(:first-of-type) {
     margin-top: 16px;
   }
 `;
-const Label = styled.label`
-  font-size: 12px;
 
-  & input {
-    display: block;
-    width: 100%;
-    padding: 10px 0 8px;
-    border: 0;
-    border-bottom: ${BORDER.basic};
-    color: ${GRAY_900};
-    font-size: 14px;
-  }
-  & input[type='file'] {
-    display: none;
-  }
-  & input::placeholder {
+const Input = styled.input`
+  display: block;
+  width: 100%;
+  padding: 10px 0 8px;
+  border: 0;
+  border-bottom: ${BORDER.basic};
+  color: ${GRAY_900};
+  font-size: 14px;
+
+  &::placeholder {
     color: ${GRAY_300};
   }
-  & input:focus,
-  & input:active {
+
+  &:focus,
+  &:active {
     border-color: ${PRIMARY};
   }
 `;
+
 const BoxUploadImg = styled.div`
   display: flex;
   align-items: center;
@@ -124,14 +124,16 @@ const BoxUploadImg = styled.div`
   height: 200px;
   margin: 18px 0 30px;
   border-radius: 10px;
-  background-color: ${SECONDARY};
+  background-color: ${GRAY_200};
 `;
-const BtnUpload = styled.button`
+
+const LabelUploadImage = styled.label`
   position: absolute;
   right: 10px;
   bottom: 10px;
   width: 36px;
   height: 36px;
   border-radius: 50%;
-  background: url('/icons/img/image.svg') no-repeat ${GRAY_600} 50% 50%;
+  background: url('/icons/img/image.svg') no-repeat ${BUTTON.background_color}
+    50% 50%;
 `;
