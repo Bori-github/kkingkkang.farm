f82b3cae40068f261fc51919d1a351c55a40633e
Bori-github <qhflrnfl4324@gmail.com>
2022-10-03 14:10:01 +0900

    💄 채팅 페이지 > 마크업 수정 및 UI 수정

--- src/components/SectionInput.tsx
+++ src/components/SectionInput.tsx
@@ -1,18 +1,25 @@
 import styled from '@emotion/styled';
 import { BORDER, BUTTON, Z_INDEX } from '../constants';
-import { WHITE } from '../constants/colors';
-import { BtnImgUpload } from './BtnImgUpload';
+import { GRAY_300, WHITE } from '../constants/colors';
 
 export const SectionInpChat = () => {
   return (
     <Contaioner>
       <h3 className="sr-only">채팅 입력</h3>
-      <BtnImgUpload />
+      <div>
+        <Label htmlFor="uploadImag">
+          <span className="sr-only">사진 업로드</span>
+        </Label>
+        <input
+          type="file"
+          id="uploadImag"
+          accept="image/*"
+          className="sr-only"
+        />
+      </div>
       <Form>
         <InpTxt type="text" placeholder="메시지 입력하기" />
-        <BtnSend type="button" className="active">
-          전송
-        </BtnSend>
+        <BtnSend type="button" disabled={true} />
       </Form>
     </Contaioner>
   );
@@ -20,7 +27,8 @@ export const SectionInpChat = () => {
 
 const Contaioner = styled.article`
   display: grid;
-  grid-template-columns: 42px auto;
+  grid-template-columns: 36px auto;
+  align-items: center;
   gap: 10px;
   position: fixed;
   right: 0;
@@ -34,7 +42,7 @@ const Contaioner = styled.article`
 
 const Form = styled.form`
   display: grid;
-  grid-template-columns: auto 50px;
+  grid-template-columns: auto 40px;
   gap: 10px;
   align-items: center;
 `;
@@ -46,12 +54,24 @@ const InpTxt = styled.input`
 `;
 
 const BtnSend = styled.button`
+  width: 40px;
+  height: 40px;
   padding: 5px;
-  border-radius: 26px;
+  border-radius: 4px;
+  background: url('/icons/post/send.svg') no-repeat 50% 50%;
   background-color: ${BUTTON.background_color};
   color: ${WHITE};
 
   &:disabled {
     background-color: ${BUTTON.disabled_color};
   }
+`;
+
+const Label = styled.label`
+  display: block;
+  width: 36px;
+  height: 36px;
+  border-radius: 50%;
+  background: url('/icons/img/image.svg') no-repeat 50% 50% ${GRAY_300};
+  cursor: pointer;
 `;
