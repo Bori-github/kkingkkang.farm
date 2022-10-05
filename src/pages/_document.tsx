254787d7cc5b727df4b8bb5f7ab715c28685b1e7
Bori-github <qhflrnfl4324@gmail.com>
2022-10-05 23:09:51 +0900

    🔍 meta tag 이미지 경로 변경

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -34,7 +34,7 @@ class MyDocument extends Document {
           />
           <meta
             property="og:image"
-            content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png"
+            content="https://mandarin.api.weniv.co.kr/1651756241213.jpg"
           />
         </Head>
         <body>
